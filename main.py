1f66865b92e31c41f9c87f312876c018499a8d5d
Bohdan <brodskyy.b@outlook.com>
2021-02-15 23:29:55 +0100

    Added main.py: main app logic and build

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("working!")
+from kivy.uix.screenmanager import Screen
+from kivymd.app import MDApp
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager
+Window.size = (375, 620)
+
+
+class MainScreen(Screen):
+    def __init__(self, **kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+
+
+class MainApp(MDApp):
+    def build(self):
+        self.screen = MainScreen(name="main_screen")
+        self.sm = ScreenManager()
+        self.sm.add_widget(self.screen)
+        return self.sm
+
+
+MainApp().run()
